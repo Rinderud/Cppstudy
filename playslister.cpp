5f418bd1d994abfbcf2fa28225be0403ddea46e0
Jacob Rinderud <jacobrinderud@example.com>
2023-06-12 23:40:04 +0200

    Mixing stage 1 done.

--- playslister.cpp
+++ playslister.cpp
@@ -19,7 +19,8 @@ song link_to_song(string link)
     return s;
 }
 
-vector<string> read_Playlist(string file_Name){
+vector<string> read_Playlist(string file_Name)
+{
     ifstream read_File(file_Name);
     vector<string> links;
     string song_Link;
@@ -35,21 +36,25 @@ int main()
 {
     vector<string> links_boris = read_Playlist("boris.txt");
     vector<song> songs_boris;
-    for (const string& i : links_boris){
+    for (const string &i : links_boris)
+    {
         songs_boris.push_back(link_to_song(i));
     }
     cout << "\nBoris songs, ids\n";
-    for (const song& i : songs_boris){
+    for (const song &i : songs_boris)
+    {
         cout << i.id << "\n";
     }
-    
+
     vector<string> links_janelle = read_Playlist("janelle.txt");
     vector<song> songs_janelle;
-    for (const string& i : links_janelle){
+    for (const string &i : links_janelle)
+    {
         songs_janelle.push_back(link_to_song(i));
     }
     cout << "\nJanelle songs, ids\n";
-    for (const song& i : songs_janelle){
+    for (const song &i : songs_janelle)
+    {
         cout << i.id << "\n";
     }
 
@@ -57,6 +62,29 @@ int main()
         1. compile list of specified length from given vector.
         2. add both compiled lists together.
     */
+    float pomo = 10.0;
+    float doro = 5.0;
+    vector<song> playlist;
+    float total = 0.0;
+    while (total < pomo)
+    {
+        song chosen_song = songs_boris.back();
+        playlist.push_back(chosen_song);
+        total += chosen_song.dur_min;
+        songs_boris.pop_back();
+    }
+    while (total < pomo+doro)
+    {
+        song chosen_song = songs_janelle.back();
+        playlist.push_back(chosen_song);
+        total += chosen_song.dur_min;
+        songs_boris.pop_back();
+    }
+    cout << "\nPomodoro songs, ids & lenghts. Total: " << total << "\n";
+    for (const song &i : playlist)
+    {
+        cout << i.id << " : " << i.dur_min << "\n";
+    }
 
     return 0;
 }
